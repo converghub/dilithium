15f9811e9f0ba3972470a607d6f1a8b62a9fe0cc
converghub <converg.ka@gmail.com>
2018-08-08 14:27:40 +0900

    Windows/MSVC compatibility

--- ref/poly.h
+++ ref/poly.h
@@ -5,9 +5,51 @@
 #include "params.h"
 #include "fips202.h"
 
+#ifdef _MSC_VER
+#    if (_MSC_VER >= 1800)
+#        define __alignas_is_defined 1
+#    endif
+#    if (_MSC_VER >= 1900)
+#        define __alignof_is_defined 1
+#    endif
+#else
+#    include <cstdalign>   // __alignas/of_is_defined directly from the implementation
+#endif
+#ifdef __alignas_is_defined
+#    define ALIGN(X) alignas(X)
+#else
+#    pragma message("C++11 alignas unsupported :( Falling back to compiler attributes")
+#    ifdef __GNUG__
+#        define ALIGN(X) __attribute__ ((aligned(X)))
+#    elif defined(_MSC_VER)
+#        define ALIGN(X) __declspec(align(X))
+#    else
+#        error Unknown compiler, unknown alignment attribute!
+#    endif
+#endif
+
+#ifdef __alignof_is_defined
+#    define ALIGNOF(X) alignof(x)
+#else
+#    pragma message("C++11 alignof unsupported :( Falling back to compiler attributes")
+#    ifdef __GNUG__
+#        define ALIGNOF(X) __alignof__ (X)
+#    elif defined(_MSC_VER)
+#        define ALIGNOF(X) __alignof(X)
+#    else
+#        error Unknown compiler, unknown alignment attribute!
+#    endif
+#endif
+
+/* Legacy
 typedef struct {
-  uint32_t coeffs[N];
+	uint32_t coeffs[N];
 } poly __attribute__((aligned(32)));
+*/
+typedef struct ALIGN(32) poly {
+	uint32_t coeffs[N];
+};
+
 
 void poly_copy(poly *b, const poly *a);
 void poly_freeze(poly *a);
